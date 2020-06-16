7074872d91ecf0aa5eba65b110109e1ecc3f905e
phd051199 <PHD051199@gmail.com>
2020-06-16 22:16:12 +0700

    up

--- TinTucGD/Controllers/NewsController.cs
+++ TinTucGD/Controllers/NewsController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TinTucGD.Models;
 
 namespace TinTucGD.Controllers
 {
     public class NewsController : Controller
     {
+        dbQLTintucDataContext data = new dbQLTintucDataContext();
         // GET: News
         public ActionResult Index()
         {
