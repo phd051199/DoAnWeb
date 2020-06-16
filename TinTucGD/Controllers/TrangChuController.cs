2a7fcca8acf4410ad56b6e06b8d477562aa62ded
phd051199 <PHD051199@gmail.com>
2020-06-16 23:42:25 +0700

    up

---
+++ TinTucGD/Controllers/TrangChuController.cs
@@ -0,0 +1,23 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TinTucGD.Models;
+
+namespace TinTucGD.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        dbQLTintucDataContext data = new dbQLTintucDataContext();
+        // GET: TrangChu
+        int pageSize = 4;
+        public ActionResult TrangChu()
+        {
+            data = new dbQLTintucDataContext();
+            ViewBag.pageCount = Math.Ceiling(1.0 * data.BAIVIETs.ToList().Count / pageSize);
+            return View();
+        }
+    }
+}
