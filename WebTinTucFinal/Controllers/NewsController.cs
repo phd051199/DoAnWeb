f9ce03888f220aa02ce47fc925479520a8d5944a
phd051199 <PHD051199@gmail.com>
2020-06-18 01:20:40 +0700

    update truoc khi di ngu

--- WebTinTucFinal/Controllers/NewsController.cs
+++ WebTinTucFinal/Controllers/NewsController.cs
@@ -11,10 +11,7 @@ namespace WebTinTucFinal.Controllers
     {
         // GET: News
         dbQLTintucDataContext data = new dbQLTintucDataContext();
-        public ActionResult Index()
-        {
-            return View();
-        }
+       
 
         public ActionResult Chuyenmuc()
         {
@@ -26,6 +23,20 @@ namespace WebTinTucFinal.Controllers
         {
             var tintuc = from t in data.BAIDANGs where t.IDBaiDang == id select t;
             return View(tintuc);
+        }
+        //Lấy tất cả bài đăng ra
+        public List<BAIDANG> AllPost()
+        {
+            return data.BAIDANGs.OrderByDescending(x => x.IDBaiDang).ToList();
+        }
+        public List<BAIDANG> MoiNhat(int count)
+        {
+            return data.BAIDANGs.OrderByDescending(x => x.NgayDang).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var allpost = AllPost();
+            return View(allpost);
         }
     }
 }
