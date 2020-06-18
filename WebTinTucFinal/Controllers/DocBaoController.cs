b706d4238596e5b9f7bd806dcfa31e62e963451b
phd051199 <PHD051199@gmail.com>
2020-06-18 21:01:25 +0700

    up

---
+++ WebTinTucFinal/Controllers/DocBaoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTinTucFinal.Models;
+
+namespace WebTinTucFinal.Controllers
+{
+    public class DocBaoController : Controller
+    {
+        dbQLTintucDataContext data = new dbQLTintucDataContext();
+        // GET: DocBao
+        public ActionResult Baiviet(int id)
+        {
+            var ndbaiviet = from ndbv in data.BAIDANGs where ndbv.IDBaiDang == id select ndbv;
+            return View(ndbaiviet);
+        }
+    }
+}
