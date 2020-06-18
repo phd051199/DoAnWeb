8f8b2bea9d2d37e0bd6b27f61388918a3a5e946b
phd051199 <PHD051199@gmail.com>
2020-06-18 16:24:15 +0700

    update

--- WebTinTucFinal/Controllers/NewsController.cs
+++ WebTinTucFinal/Controllers/NewsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebTinTucFinal.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.Threading;
 
 namespace WebTinTucFinal.Controllers
 {
@@ -28,21 +31,28 @@ namespace WebTinTucFinal.Controllers
         {
             return data.BAIDANGs.OrderByDescending(x => x.NgayDang).ToList();
         }
- 
-        public ActionResult Index()
+        public List<BAIDANG> TheoTheLoai(int idtl)
         {
+            return data.BAIDANGs.OrderBy(x => x.IDTheLoai).Take(idtl).ToList();
+        }
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 4;
+            int pageNum = (page ?? 1);
             var allpost = AllPost();
-            return View(allpost);
+            return View(allpost.ToPagedList(pageNum, pageSize));
         }
         public ActionResult Baidangmoinhat()
         {
             var moinhat = MoiNhat(4);
-            return PartialView(moinhat);
+            return View(moinhat);
         }
         //Tin theo the loai
        public ActionResult TinTheoTheLoai(int id)
         {
             var baiviet = from bv in data.BAIDANGs where bv.IDTheLoai == id select bv;
+            //var baiviettheotheloai = TheoTheLoai(id);
+            //ViewBag.tintheotl= TheoTheLoai(id);
             return View(baiviet);
         }
 
