c7fdc4245112205078bbcafc04d114c02b73fc7b
phd051199 <PHD051199@gmail.com>
2020-06-18 13:07:08 +0700

    update

---
+++ WebTinTucFinal/Controllers/DocBaoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTinTucFinal.Models;
+
+namespace WebTinTucFinal.Controllers
+{
+    public class DocBaoController : Controller
+    {
+        // GET: DocBao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
