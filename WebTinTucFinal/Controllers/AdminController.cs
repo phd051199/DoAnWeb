ef23873848a49ec34eaa0281b967c12afa9e1481
phd051199 <PHD051199@gmail.com>
2020-06-19 06:41:10 +0700

    update

--- WebTinTucFinal/Controllers/AdminController.cs
+++ WebTinTucFinal/Controllers/AdminController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using WebTinTucFinal.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.Security.Cryptography;
 
 namespace WebTinTucFinal.Controllers
 {
@@ -16,6 +20,86 @@ namespace WebTinTucFinal.Controllers
         {
             return View();
         }
+
+        //hien thi chi tiet
+        public ActionResult Chitietbai(int id)
+        {
+            BAIDANG baidang = db.BAIDANGs.SingleOrDefault(n => n.IDBaiDang == id);
+            ViewBag.IDBaiDang = baidang.IDBaiDang;
+            if(baidang==null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(baidang);
+        }
+        //xoa bai viet
+        [HttpPost,ActionName ("XoaBaiViet")]
+        public ActionResult Xacnhanxoa(int id)
+        {
+            BAIDANG baidang = db.BAIDANGs.SingleOrDefault(n => n.IDBaiDang == id);
+            ViewBag.IDBaiDang = baidang.IDBaiDang;
+            if (baidang==null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.BAIDANGs.DeleteOnSubmit(baidang);
+            db.SubmitChanges();
+            return RedirectToAction("BaiViet");
+        }
+        //
+        public ActionResult BaiViet(int ?page)
+        {
+            int pageSize = 5;
+            int pageNum = (page ?? 1);
+            return View(db.BAIDANGs.ToList().OrderBy(n=>n.IDBaiDang).ToPagedList(pageNum, pageSize));
+        }
+
+        [HttpGet]
+        public ActionResult VietBai()
+        {
+            ViewBag.IDTheLoai = new SelectList(db.THELOAIs.ToList().OrderBy(n => n.TenTheLoai), "IDTheLoai", "TenTheLoai");
+            ViewBag.IDTaiKhoan = new SelectList(db.TAIKHOANs.ToList().OrderBy(n => n.TenTaiKhoan), "IDTaiKhoan", "TenTaiKhoan");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult VietBai(BAIDANG baidang, HttpPostedFileBase fileupload)
+        {
+            ViewBag.IDTheLoai = new SelectList(db.THELOAIs.ToList().OrderBy(n => n.TenTheLoai), "IDTheLoai", "TenTheLoai");
+            ViewBag.IDTaiKhoan = new SelectList(db.TAIKHOANs.ToList().OrderBy(n => n.TenTaiKhoan), "IDTaiKhoan", "TenTaiKhoan");
+            if (fileupload == null)
+            {
+                ViewBag.ThongBao = "Vui lòng chọn ảnh bìa !";
+            }
+            else 
+            {
+                if (ModelState.IsValid)
+                {
+                    var filename = Path.GetFileName(fileupload.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/images"), filename);
+                    if (System.IO.File.Exists(path))
+                    {
+                        ViewBag.ThongBao = "Hình ảnh đã tồn tại";
+
+                    }
+                    else
+                    {
+                        fileupload.SaveAs(path);
+                    }
+                    baidang.AnhDaiDien = filename;
+                    db.BAIDANGs.InsertOnSubmit(baidang);
+                    db.SubmitChanges();
+                }
+                return RedirectToAction("BaiViet");
+            }
+            return View();
+           
+        }
+
+
         [HttpGet]
         public ActionResult Dangnhap()
         {
@@ -23,7 +107,6 @@ namespace WebTinTucFinal.Controllers
         }
 
         [HttpPost]
-
         public ActionResult Dangnhap(FormCollection collection)
         {
             var tendn = collection["Username"];
